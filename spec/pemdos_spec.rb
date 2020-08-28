890f4e21bd2521ce91adef72e0ffe0fd0b40ea0f
Zachary Jacob Rodriguez <zjrodrig8@gmail.com>
2020-08-28 17:11:00 +0000

    Automatically backed up by Learn

--- spec/pemdos_spec.rb
+++ spec/pemdos_spec.rb
@@ -4,6 +4,5 @@ require 'pry'
 describe "pemdos" do
   it "`snake_it_up` adds the letter 's' ten times before any word that starts with 's'" do
   expect(snake_it_up("surprise!")).to eq("sssssssssssurprise!")
-  binding.pry
   end
 end
